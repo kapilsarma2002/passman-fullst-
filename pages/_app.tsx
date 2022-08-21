7509eb4f07af4fc33fd542d0849770d2f813bc4d
kapilsarma2002 <sarmakapil1818@gmail.com>
2022-08-21 11:33:54 +0530

    route-protect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import PageLayout from '../components/pageLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <ChakraProvider>
+      {
+        Component.authPage ? (
+          <Component {...pageProps} />
+        ) : (
+          <PageLayout>
+            <Component {...pageProps} />
+          </PageLayout>
+        )
+      }
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
